47ee64518107a1bc20e1db48fa2f289673091c3f
jeremy.zhang <zhangyue9306@gmail.com>
2021-02-01 22:35:28 +0800

    add lk list

---
+++ cpp/cpp-practice/linkedlist/linkedlist/main.cpp
@@ -0,0 +1,129 @@
+//
+//  main.cpp
+//  linkedlist
+//
+//  Created by Yue Zhang on 9/1/21.
+//
+
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next; // pointer to next node
+};
+
+class LinkedList {
+    Node* head;
+    Node* tail;
+public:
+    LinkedList() {
+        head = NULL;
+        tail = NULL;
+    }
+    
+    // O(1)
+    void insert_end(int data) {
+        Node* new_node = new Node;
+        new_node->data = data;
+        new_node->next = NULL;
+        if (head == NULL) {
+            head = new_node;
+            tail = new_node;
+        } else {
+            tail->next = new_node;
+            tail = new_node;
+        }
+    }
+    
+    void insert_start(int data) {
+        if (isEmpty()) insert_end(data);
+        Node* new_node = new Node;
+        new_node->data = data;
+        new_node->next = head;
+        head = new_node;
+    }
+    
+    // O(n)
+    void insert_position(int pos, int data) {
+        if (pos == 0) insert_start(data);
+
+        Node* node = head;
+        while (pos > 1) {
+            node = node->next;
+            pos--;
+        }
+        Node* new_node = new Node;
+        new_node->data = data;
+        new_node->next = node->next;
+        node->next = new_node;
+    }
+    
+    void delete_first() {
+        if (isEmpty()) return;
+        Node* tmp = head;
+        head = head->next;
+        delete tmp;
+    }
+    
+    // O(n)
+    void delete_last() {
+        if (isEmpty()) return;
+        Node* node = head;
+        Node* prev = new Node;
+        while (node->next) {
+            prev = node;
+            node = node->next;
+        }
+        prev->next = NULL;
+        tail = prev;
+        delete node;
+    }
+    
+    // O(n)
+    void delete_position(int pos) {
+        if (isEmpty()) return;
+        if (pos == 0) delete_first();
+        
+        Node* node = head;
+        while (pos > 1) {
+            node = node->next;
+            pos--;
+        }
+        node->next = node->next->next;
+        delete node->next;
+    }
+    
+    bool isEmpty() {
+        if (head) {
+            return false;
+        }
+        return true;
+    }
+    
+    void printList() {
+        if (isEmpty()) return;
+        Node* node = head;
+        while (node) {
+            cout << node->data << " ";
+            node = node->next;
+        }
+        cout << endl;
+    }
+};
+// malloc allocates memory, while new allocates memory AND calls the constructor of the object you're allocating memory for.
+int main() {
+    // insert code here...
+    LinkedList a;
+    a.insert_end(3);
+    a.insert_start(1);
+    a.insert_end(4);
+    a.insert_start(5);
+    a.insert_position(1, 20);
+    a.delete_first();
+    a.delete_last();
+    a.delete_position(1);
+    a.printList();
+    
+    return 0;
+}
