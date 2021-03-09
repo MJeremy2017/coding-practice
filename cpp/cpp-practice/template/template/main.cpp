0e7c82798c3f42ebe7fd9bce3e09d7f442fa6871
jeremy.zhang <zhangyue9306@gmail.com>
2021-03-09 22:49:06 +0800

    add copy and deep copy

--- cpp/cpp-practice/template/template/main.cpp
+++ cpp/cpp-practice/template/template/main.cpp
@@ -24,12 +24,61 @@ public:
     }
 };
 
+class MyString {
+private:
+    char* buffer;
+    unsigned int size;
+public:
+    MyString(const char* s) {
+        size = int(strlen(s));
+        buffer = new char[size+1];
+        memcpy(buffer, s, size);
+        // add end character
+        buffer[size] = 0;
+    }
+
+    char& operator[](int index) {
+        return buffer[index];
+    }
+    
+    // copy constructor
+    MyString(const MyString& another) {
+        cout << "string is copied" << endl;
+        // to perform a deep copy
+        size = another.size;
+        buffer = new char[size+1];
+        memcpy(buffer, another.buffer, size+1);
+    }
+
+    ~MyString() {
+        delete buffer;
+    }
+        
+    friend ostream& operator<<(ostream& stream, const MyString& s);
+};
+
+// if not with '&
+void printMyString(MyString& s) {
+    cout << s << endl;
+}
+
+ostream& operator<<(ostream& stream, const MyString& s) {
+    stream << s.buffer;
+    return stream;
+}
+
 int main(int argc, const char * argv[]) {
     // insert code here...
 //    print_value(53);
 //    print_value("addidas");
+    MyString a = "asdfsd";
+    MyString b = a;
+    
+    cout << a << " " << b << endl;
+    printMyString(a);
     
     Array<int, 10> arr;
     cout << arr.get_size() << endl;
+    
     return 0;
 }
