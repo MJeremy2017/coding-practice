3526c034cbc54d475f9d54b5dfe8b8b975c8bde4
jeremy.zhang <zhangyue9306@gmail.com>
2021-03-11 23:08:20 +0800

    add arrow

--- cpp/cpp-practice/arrow/arrow/main.cpp
+++ cpp/cpp-practice/arrow/arrow/main.cpp
@@ -6,6 +6,7 @@
 //
 
 #include <iostream>
+#include <vector>
 using namespace std;
 
 class Entity {
@@ -17,6 +18,11 @@ public:
     
     void Print() {
         cout << "Entity x: " << x << endl;
+    }
+    
+    Entity(const Entity& other)
+    :  x(other.x) {
+        cout << "copy happened" << endl;
     }
 };
 
@@ -38,11 +44,62 @@ public:
     }
 };
 
+
+vector<int> decToBinary(int n)
+{
+    int len = 0;
+    int arr[32];
+    while (n) {
+        arr[len] = n%2;
+        n /= 2;
+        len++;
+    }
+    
+    vector<int> res;
+    for (int i=len-1; i>=0; --i) {
+        res.push_back(arr[i]);
+    }
+    return res;
+}
+
+void Binary2dec(vector<int> v) {
+    int factor = 1;
+    int res = 0;
+    for (int i=0; i<v.size(); ++i) {
+        res += v[i] * factor;
+        factor *= 2;
+    }
+    cout << res << endl;
+}
+
 int main(int argc, const char * argv[]) {
     // insert code here...
     ScopedPtr sp = new Entity(12);
-    
     // -> overloaded a-> == a.operator->()
     sp->Print();
+    
+    // vectirs
+    vector<Entity> v;
+    // 3 copies
+    // construct entity and place into the vector makes a copy, expanding vector makes another copy
+    v.push_back(Entity(2));
+    v.push_back(Entity(3));
+    
+    cout << "-------" << endl;
+    
+    // 2 copies
+    vector<Entity> v2;
+    v2.reserve(2);
+    v2.push_back(Entity(2));
+    v2.push_back(Entity(3));
+
+    cout << "-------" << endl;
+    
+    // 0 copies
+    vector<Entity> v3;
+    v3.reserve(2);
+    v3.emplace_back(2);
+    v3.emplace_back(3);
+    
     return 0;
 }
