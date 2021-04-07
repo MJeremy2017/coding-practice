490f2d1d4ecaa1b14c167a6a87254384484b3522
jeremy.zhang <zhangyue9306@gmail.com>
2021-04-07 22:24:57 +0800

    add virtual dest

---
+++ cpp/cpp-practice/virtual_destructor/virtual_destructor/main.cpp
@@ -0,0 +1,51 @@
+//
+//  main.cpp
+//  virtual_destructor
+//
+//  Created by Yue Zhang on 7/4/21.
+//
+
+#include <iostream>
+using namespace std;
+
+class Base {
+public:
+    Base() {
+        cout << "Base Constructor" << endl;
+    }
+    
+    // indicate that this destructor could be extended
+    virtual ~Base() {
+        cout << "Base Destructor" << endl;
+    }
+};
+
+class Derived : public Base {
+public:
+    Derived() {
+        cout << "Derived Constructor" << endl;
+    }
+
+    ~Derived() {
+        cout << "Derived Destructor" << endl;
+    }
+
+};
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    Base* base = new Base;
+    delete base;
+    
+    cout << "---------------------" << endl;
+    
+    Derived* derived = new Derived;
+    delete derived;
+
+    cout << "---------------------" << endl;
+    
+    Base* poly = new Derived;
+    delete poly;
+
+    return 0;
+}
